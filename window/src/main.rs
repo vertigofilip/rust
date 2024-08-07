0332946fa5aacfbfdd1a076b96aba3e0ddd991b9
vertigo.filip <44265838+vertigofilip@users.noreply.github.com>
2024-08-07 20:52:32 +0200

    aa

---
+++ window/src/main.rs
@@ -0,0 +1,31 @@
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+
+    ui.on_request_decrise_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() - 1);
+        }
+    });
+
+    ui.on_request_set_value_100({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(100);
+        }
+    });
+
+    ui.run()
+}
